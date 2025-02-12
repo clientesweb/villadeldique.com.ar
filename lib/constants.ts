dfe5f1fe40ffd3bbd58969bea5fd990547a9b0f8
clientesweb <alexisifran32@gmail.com>
2025-02-12 08:05:48 -0300

    Actualizar constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -19,6 +19,7 @@ export const SOCIAL_LINKS = {
   facebook: "https://www.facebook.com/villadeldiquedigital",
   instagram: "https://www.instagram.com/villadeldiquedigital",
   twitter: "https://twitter.com/villadeldiquedigital",
+  youtube: "https://www.youtube.com/channel/UCxxxxxxxxxxxxxxx", // Replace with the actual YouTube channel URL
 }
 
 export const CONTACT_INFO = {
@@ -35,5 +36,15 @@ export const CATEGORIES = [
 ]
 
 export const ARTICLES: Article[] = [
-  
+  {
+    id: 1,
+    slug: "sample-article",
+    title: "Sample Article",
+    description: "This is a sample article",
+    image: "/placeholder.svg",
+    fullContent: "This is the full content of the sample article.",
+    category: "noticias",
+  },
+  // Add more articles as needed
 ]
+
