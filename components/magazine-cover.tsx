a56ff05ac2f52a3fd58a9c65543261f5bef5e44e
clientesweb <alexisifran32@gmail.com>
2025-02-20 03:15:12 -0300

    Actualizar magazine-cover.tsx

--- components/magazine-cover.tsx
+++ components/magazine-cover.tsx
@@ -20,7 +20,7 @@ export default function MagazineCover() {
   return (
     <div className="relative h-screen w-full overflow-hidden">
       <Image
-        src="https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1920&q=80"
+        src="/hero-villa-del-dique-lago.jpg"
         alt="Villa Del Dique"
         layout="fill"
         objectFit="cover"
