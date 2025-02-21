bb8723574664a396756f5191118df0f5dd72001c
clientesweb <alexisifran32@gmail.com>
2025-02-20 21:47:06 -0300

    Actualizar next.config.js

--- next.config.js
+++ next.config.js
@@ -1,40 +1,25 @@
-const withPWA = require("next-pwa")({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
-})
-
-const path = require("path")
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  output: "standalone",
   images: {
     domains: ["images.unsplash.com"],
     unoptimized: true,
   },
-  webpack: (config) => {
-    config.resolve.alias["@"] = path.resolve(__dirname)
-    return config
-  },
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-  },
-  async redirects() {
+  reactStrictMode: true,
+  async headers() {
     return [
       {
-        source: "/categoria/:slug",
-        destination: "/:slug",
-        permanent: true,
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
       },
     ]
   },
 }
 
-module.exports = withPWA(nextConfig)
+module.exports = nextConfig
 
