d059de06f94a0a828cc460754797a9c015caf085
clientesweb <alexisifran32@gmail.com>
2025-02-21 01:25:32 -0300

    Actualizar next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: "standalone",
   images: {
-    domains: [],
+    domains: ["images.unsplash.com"], // Añada aquí otros dominios si es necesario
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
@@ -14,48 +14,6 @@ const nextConfig = {
   reactStrictMode: true,
   compress: true,
   poweredByHeader: false,
-  async headers() {
-    return [
-      {
-        source: "/:all*(svg|jpg|png|webp|avif)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-      {
-        source: "/_next/image/:all*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-      {
-        source: "/:all*(js|css)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ]
-  },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      config.optimization.splitChunks.cacheGroups.styles = {
-        name: "styles",
-        test: /\.(css|scss)$/,
-        chunks: "all",
-        enforce: true,
-      }
-    }
-    return config
-  },
 }
 
 module.exports = nextConfig
