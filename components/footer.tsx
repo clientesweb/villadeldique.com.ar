a2fbf4678902e288841dcc3f23c3f3d9dd5de304
clientesweb <alexisifran32@gmail.com>
2025-02-21 01:29:18 -0300

    Actualizar footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -27,7 +27,7 @@ export default function Footer() {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           <div className="flex flex-col items-center md:items-start">
             <Image
-              src="/images/logo-villa-del-dique-digital.webp"
+              src="/logo-villa-del-dique-digital.png"
               alt="Villa del Dique Digital"
               width={150}
               height={60}
