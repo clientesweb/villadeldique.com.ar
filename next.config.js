4c72b5bcb4a9baf4ec8ed258dd34d4abf977f1d6
clientesweb <alexisifran32@gmail.com>
2025-02-21 01:32:28 -0300

    Actualizar next.config.js

--- next.config.js
+++ next.config.js
@@ -2,10 +2,7 @@
 const nextConfig = {
   output: "standalone",
   images: {
-    domains: ["images.unsplash.com"], // Añada aquí otros dominios si es necesario
-    formats: ["image/avif", "image/webp"],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    domains: [], // Eliminamos los dominios externos ya que usaremos imágenes locales
   },
   experimental: {
     optimizeCss: true,
