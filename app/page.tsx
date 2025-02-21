fcd055ab0e4341106c8a5aede13425769c92b835
clientesweb <alexisifran32@gmail.com>
2025-02-21 01:07:58 -0300

    Actualizar page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -63,7 +63,7 @@ export default function UnderConstructionPage() {
       <div className="relative z-10 max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
         <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }}>
           <Image
-            src="/logo-villa-del-dique-digital.png"
+            src="http://villadeldique.com.ar/logo-villa-del-dique-digital.png"
             alt="Villa del Dique Digital"
             width={300}
             height={100}
@@ -165,4 +165,3 @@ export default function UnderConstructionPage() {
     </div>
   )
 }
-
